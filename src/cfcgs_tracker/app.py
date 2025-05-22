f1c12e23dccc5ca68047bf0025ea60ab57f77cad
jordany <jordanyluiz@gmail.com>
2025-05-21 23:33:08 -0300

    feat[funds]: implementando o mvp dos módulos fund, fund_type e fund_focus

--- src/cfcgs_tracker/app.py
+++ src/cfcgs_tracker/app.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 from http import HTTPStatus
 from src.cfcgs_tracker.schemas import Message
+from src.cfcgs_tracker.routers import funds, fund_types, fund_focuses
+
 app = FastAPI()
 
-
+app.include_router(funds.router)
+app.include_router(fund_types.router)
+app.include_router(fund_focuses.router)
 @app.get("/", status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {"message": "Hello World"}
+
